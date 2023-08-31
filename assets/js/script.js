3e18c375e08a6d37ccca154869afdccc7f931ea5
tadeubdossantos <tadeub.dossantos@gmail.com>
2023-08-31 16:18:12 -0300

    inclusao comentarios no scrip

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,20 @@ const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
+/* A cada 1 segundo vai ser executado essa função */
 const relogio = setInterval(function time() {
+    /* Obtém o momento em horas, minutos e segundos */
     let dateToday = new Date();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let s = dateToday.getSeconds();
 
+    /* Formata os dados de tempo obtidos */
     if(hr < 10) hr = '0' + hr;
     if(min < 10) min = '0' + min;
     if(s < 10) s = '0' + s;
 
+    /* Renderiza o horário atual */
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = s;
